c1da86e7a340441c790369b20a6c19b9907dd77d
Gerhard Muntingh <gerhard@qux.nl>
2014-10-04 08:56:48 +0200

    big red patch

--- button.h
+++ button.h
@@ -1,14 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <libusb-1.0/libusb.h>
 
 static const int CONTROL_REQUEST_TYPE_IN = 0x21;
 static const int HID_SET_REPORT = 0x09;
 static const int HID_REPORT_TYPE_OUTPUT = 0x02;
-static const char HID_CTRL_DATA[8] = {0,0,0,0,0,0,0,2};
 static const int BUTTON_STATE_CLOSED = 0x15;
-static const int BUTTON_STATE_OPENED = 0x17;
 static const int BUTTON_STATE_PRESSED = 0x16;
+static const int BUTTON_STATE_OPENED = 0x17;
+static char HID_CTRL_DATA[8] = {0,0,0,0,0,0,0,2};
 
 struct libusb_device_handle *openButton();
 unsigned char getButtonState(struct libusb_device_handle *dev);
--- main.c
+++ main.c
@@ -1,44 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <libusb-1.0/libusb.h>
 #include "button.h"
 
-int main() {
-    struct libusb_device_handle *dev = openButton();
-    int state = 0;
-    do {
-        int val = getButtonState(dev);
-        if(val != state) {
-            if(val == 1) {
-                state = val;
-                if(fork() == 0) {
-                    system("panicbutton-opened.sh");
-                    return 0;
-                }
-                else printf("Opened.\n");
-            }
-            else if(val == 2) {
-                if(fork() == 0) {
-                    system("panicbutton-pressed.sh");
-                    return 0;
-                }
-                else printf("Pressed.\n");
-                while(getButtonState(dev) == 2) usleep(100000);
-            }
-            else {
-                state = val;
-                if(fork() == 0) {
-                    system("panicbutton-closed.sh");
-                    return 0;
-                }
-                else printf("Closed.\n");
-            }
-        }
-        usleep(100000);
-    } while(1);
-    if(libusb_release_interface(dev, 0) != 0) {
-        printf("Cannot unclaim button\n");
-    }
+int main()
+{
+	struct libusb_device_handle *dev = openButton();
 
-    libusb_close(dev);
-    return 0;
+	if (!dev) {
+		exit(1);
+	}
+
+	/* Remember if the lid is open */
+	int state = BUTTON_STATE_CLOSED;
+
+	while (1) {
+		int val = getButtonState(dev);
+
+		if (val != state) {
+
+			if (val == BUTTON_STATE_OPENED) {
+				state = val;
+
+				if (fork() == 0) {
+					system("panicbutton-opened.sh");
+					exit(0);
+				}
+
+				fprintf(stderr, "Opened.\n");
+			}
+			else if (val == BUTTON_STATE_PRESSED) {
+
+				if (fork() == 0) {
+					system("panicbutton-down.sh");
+					exit(0);
+				}
+
+				fprintf(stderr, "Button down.\n");
+
+				/* Wait until the button is up again */
+				while (getButtonState(dev) == BUTTON_STATE_PRESSED) usleep(100000);
+
+				if (fork() == 0) {
+					system("panicbutton-up.sh");
+					exit(0);
+				}
+
+				fprintf(stderr, "Button up.\n");
+
+			}
+			else if (val == BUTTON_STATE_CLOSED) {
+				state = val;
+
+				if (fork() == 0) {
+					system("panicbutton-closed.sh");
+					exit(0);
+				}
+
+				fprintf(stderr, "Closed.\n");
+			}
+			else if (val == 20) {
+				fprintf(stderr, "Button pushed while the lid is closed?\n");
+			}
+			else {
+				fprintf(stderr, "Unknown button status %d\n", val);
+			}
+		}
+
+		usleep(100000);
+	};
+
+	if (libusb_release_interface(dev, 0) != 0) {
+		printf("Cannot unclaim button\n");
+	}
+
+	libusb_close(dev);
+
+	return 0;
 }
